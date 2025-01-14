4931ddc2d34bd493ee8c67c340e40f1785b3ebf6
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-01-14 13:01:58 +0200

    [2657] [Time Beats: 8.33%] [Memory Beats: 40.89%] - LeetPush

--- 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.java
+++ 2657-Find-the-Prefix-Common-Array-of-Two-Arrays.java
@@ -4,20 +4,14 @@ class Solution {
         int[] res=new int[n];
         Map<Integer, Integer> hashtable = new HashMap<>();
         for(int i=0;i<n;i++){
-            if(hashtable.containsKey(A[i])){
-                hashtable.put(A[i], hashtable.get(A[i])+1);
-            } else {
-                hashtable.put(A[i], 1);
-            }
+            if(hashtable.containsKey(A[i])) hashtable.put(A[i], hashtable.get(A[i])+1);
+            else hashtable.put(A[i], 1);
 
-            if(hashtable.containsKey(B[i])){
-                hashtable.put(B[i], hashtable.get(B[i])+1);
-            } else {
-                hashtable.put(B[i], 1);
-            }
+            if(hashtable.containsKey(B[i])) hashtable.put(B[i], hashtable.get(B[i])+1);
+            else hashtable.put(B[i], 1);
 
             int count=0;
-            for (Integer key : hashtable.keySet()) {
+            for (Integer key: hashtable.keySet()) {
                 if(hashtable.get(key)>=2) count++;
             }
             res[i]=count;
