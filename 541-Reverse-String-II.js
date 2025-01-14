4a28a8f121b25416b588a88d5ed70e2f5d358e83
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-01-14 19:25:31 +0200

    [541] [Time Beats: 24.63%] [Memory Beats: 26.11%] - LeetPush

--- 541-Reverse-String-II.js
+++ 541-Reverse-String-II.js
@@ -5,7 +5,7 @@
  */
 var reverseStr = function(s, k) {
     let arr = s.split('');
-    for (let i = 0; i < arr.length; i += 2 * k) {
+    for (let i=0; i<arr.length; i+=2*k) {
         let l=i, r=i+k-1;
         while (l<r){
             [arr[r], arr[l]] = [arr[l], arr[r]];
