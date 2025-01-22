d9a2093a7ab80ae19e46fc59277ca987cbf36549
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-01-22 14:07:46 +0200

    [1863] [Time Beats: 10.77%] [Memory Beats: 6.15%] - LeetPush

---
+++ 1863-Sum-of-All-Subset-XOR-Totals.ts
@@ -0,0 +1,15 @@
+function subsetXORSum(nums: number[]): number {
+    let cnt=0;
+    const subsets: number[][] = [[]];
+    for (const el of nums) {
+        const last = subsets.length-1;
+        for (let i = 0; i <= last; i++) {
+        subsets.push([...subsets[i], el]);
+        }
+    }
+
+    for (const subset of subsets) {
+        cnt += subset.reduce((a,b)=>a^b, 0)
+    }
+    return cnt;
+};
