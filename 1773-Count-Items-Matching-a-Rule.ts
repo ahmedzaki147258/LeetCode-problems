e6863e2ff153a281186bd270cfcd62986f994ce5
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-01-31 11:11:36 +0200

    [1773] [Time Beats: 26.19%] [Memory Beats: 57.14%] - LeetPush

--- 1773-Count-Items-Matching-a-Rule.ts
+++ 1773-Count-Items-Matching-a-Rule.ts
@@ -1,13 +1,9 @@
 function countMatches(items: string[][], ruleKey: string, ruleValue: string): number {
     let cnt=0;
     for (let i=0;i<items.length;i++) {
-        if(ruleKey==="type"){
-            if(ruleValue===items[i][0]) cnt++;
-        } else if(ruleKey==="color"){
-            if(ruleValue===items[i][1]) cnt++;
-        } else if(ruleKey==="name"){
-            if(ruleValue===items[i][2]) cnt++;
-        }
+        if(ruleKey==="type" && ruleValue===items[i][0]) cnt++;
+        else if(ruleKey==="color" && ruleValue===items[i][1]) cnt++;
+        else if(ruleKey==="name" && ruleValue===items[i][2]) cnt++;
     }
     return cnt;
 };
