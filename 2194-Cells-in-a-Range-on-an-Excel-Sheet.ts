4905e013fbd0d0c301e621a21a05b3b57195a01b
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-07 22:05:29 +0200

    [2194] [Time Beats: 50.00%] [Memory Beats: 7.89%] - LeetPush

--- 2194-Cells-in-a-Range-on-an-Excel-Sheet.ts
+++ 2194-Cells-in-a-Range-on-an-Excel-Sheet.ts
@@ -1,17 +1,13 @@
 function cellsInRange(s: string): string[] {
-     const result: string[]=[];
-  const firstLetter=s.charCodeAt(0);
-  const endLetter=s.charCodeAt(3);
-  const start=+s[1];
-  const end=+s[4];
-  for(let i=firstLetter;i<=endLetter;i++){
-    // console.log(String.fromCharCode(i));
-    for (let j=start;j<=end;j++) {
-      result.push(String.fromCharCode(i)+j);
+    const result: string[]=[];
+    const firstLetter=s.charCodeAt(0);
+    const endLetter=s.charCodeAt(3);
+    const start=+s[1];
+    const end=+s[4];
+    for(let i=firstLetter;i<=endLetter;i++){
+        for (let j=start;j<=end;j++) {
+            result.push(String.fromCharCode(i)+j);
+        }
     }
-    
-  }
-  // console.log(firstLetter, endLetter);
-  // console.log(start, end);
-  return result;
+    return result;
 };
