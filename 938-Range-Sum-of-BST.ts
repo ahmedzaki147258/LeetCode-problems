d983c38eb9d9daec0f1ff24a76e5eed3b0371859
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-07 20:57:00 +0200

    [938] [Time Beats: 100.00%] [Memory Beats: 5.71%] - LeetPush

--- 938-Range-Sum-of-BST.ts
+++ 938-Range-Sum-of-BST.ts
@@ -13,8 +13,8 @@
  */
 
 function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
-    if(root===null) return 0;
     let sum=0;
+    if(root===null) return 0;
     if(root.val>=low && root.val<=high) sum+=root.val;
     if(root.val>low) sum+=rangeSumBST(root.left, low, high);
     if(root.val<high) sum+=rangeSumBST(root.right, low, high);
