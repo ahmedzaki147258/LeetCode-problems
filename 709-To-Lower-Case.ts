9c0ce5b412baaf2ab989f4f147aee782c3201096
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-07 22:17:22 +0200

    [709] [Time Beats: 100.00%] [Memory Beats: 15.96%] - LeetPush

--- 709-To-Lower-Case.ts
+++ 709-To-Lower-Case.ts
@@ -1,3 +1,12 @@
 function toLowerCase(s: string): string {
-    return s.toLowerCase();
+    let res="";
+    for (let i=0;i<s.length;i++) {
+        if(s.charCodeAt(i)>=65 && s.charCodeAt(i)<=90){
+            res+=String.fromCharCode(s.charCodeAt(i)+32);
+        } else {
+            res+=s[i];
+        }
+    }
+    return res;
+    // return s.toLowerCase();
 };
