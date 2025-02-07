1421af3aa220d50e067272156b914f07eda6926f
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-07 20:42:02 +0200

    [2037] [Time Beats: 63.49%] [Memory Beats: 55.56%] - LeetPush

--- 2037-Minimum-Number-of-Moves-to-Seat-Everyone.ts
+++ 2037-Minimum-Number-of-Moves-to-Seat-Everyone.ts
@@ -2,8 +2,5 @@ function minMovesToSeat(seats: number[], students: number[]): number {
     let cnt=0;
     seats.sort((a,b)=>a-b);
     students.sort((a,b)=>a-b);
-    for (let i=0;i<students.length;i++) {
-        cnt+=Math.abs(seats[i]-students[i]);
-    }
-    return cnt;
+    return students.reduce((prev, curr, idx)=>prev+Math.abs(curr-seats[idx]), 0);
 };
