f92a56834cecfbd42443eb98a13973ff1ee02d5a
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-14 16:20:57 +0200

    [2181] [Time Beats: 29.22%] [Memory Beats: 43.68%] - LeetPush

---
+++ 2181-Merge-Nodes-in-Between-Zeros.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        ListNode begin=new ListNode();
+        ListNode res=begin;
+        ListNode curr=head.next;
+
+        while (curr!=null) {
+            int sum=0;
+            while (curr!=null && curr.val!=0) {
+                sum+=curr.val;
+                curr=curr.next;
+            }
+            res.next=new ListNode(sum);
+            res=res.next;
+            curr=curr.next;
+        }
+        return begin.next;
+    }
+}
