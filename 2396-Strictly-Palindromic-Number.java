82ee6c5ed43fe376b90027afb5b5d9a88bbfa004
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-14 16:42:36 +0200

    [2396] [Time Beats: 100.00%] [Memory Beats: 43.25%] - LeetPush

--- 2396-Strictly-Palindromic-Number.java
+++ 2396-Strictly-Palindromic-Number.java
@@ -1,5 +1,20 @@
 class Solution {
     public boolean isStrictlyPalindromic(int n) {
-        return false;
+        for (int i=2;i<=n-2;i++) {
+            if(!convertAndCheck(n,i)) return false;
+        }
+        return true;
+    }
+    public static boolean convertAndCheck(int number, int base) {
+        StringBuilder res = new StringBuilder();
+        while (number > 0) {
+            res.insert(0, number % base);
+            number /= base;
+        }
+        int n=res.length();
+        for (int i = 0; i < n; i++) {
+            if(res.charAt(i) != res.charAt(n-i-1)) return false;
+        }
+        return true;
     }
 }
