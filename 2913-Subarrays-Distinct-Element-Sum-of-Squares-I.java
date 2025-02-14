58da5e8cea2275f14688d93190a14c90850d0817
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-14 13:59:07 +0200

    [2913] [Time Beats: 93.52%] [Memory Beats: 93.93%] - LeetPush

--- 2913-Subarrays-Distinct-Element-Sum-of-Squares-I.java
+++ 2913-Subarrays-Distinct-Element-Sum-of-Squares-I.java
@@ -1,8 +1,9 @@
 class Solution {
     public int sumCounts(List<Integer> nums) {
         int sum=0; final int n=nums.size();
+        Set<Integer> set=new HashSet<>();
         for (int i = 0; i < n; i++) {
-            Set<Integer> set=new HashSet<>();
+            set.clear();
             for (int j = i; j < n; j++) {
                 set.add(nums.get(j));
                 sum+=Math.pow(set.size(), 2);
