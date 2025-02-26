e9f44eb588cce4976b7b696d25711ca927a8a88a
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-02-26 06:12:17 +0200

    [1661] [Time Beats: 56.94%] - LeetPush

---
+++ 1661-Average-Time-of-Process-per-Machine.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+-- with temporaryTable AS (
+--     select machine_id
+-- )
+select a.machine_id, cast(avg(b.timestamp-a.timestamp) as decimal(10,3)) as processing_time
+from Activity a
+join Activity b
+where a.machine_id=b.machine_id AND a.process_id=b.process_id AND a.activity_type='start' AND b.activity_type='end'
+group by a.machine_id
