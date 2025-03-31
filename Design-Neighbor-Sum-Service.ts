0e5d9a1d8dd74299a8ff6daee76ab84d7a45b5a5
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-04-01 00:22:55 +0200

    [3242] [Time Beats: 58.06%] [Memory Beats: 61.29%] - LeetPush

---
+++ Design-Neighbor-Sum-Service.ts
@@ -0,0 +1,47 @@
+class NeighborSum {
+  n: number;
+  grid: number[][];
+
+  constructor(grid: number[][]) {
+    this.n = grid.length;
+    this.grid = grid;
+  }
+
+  adjacentSum(value: number): number {
+    let sum = 0;
+    const [x, y] = this.getIndex(value);
+    if (y + 1 < this.n) sum += this.grid[x][y + 1];
+    if (y - 1 >= 0) sum += this.grid[x][y - 1];
+    if (x + 1 < this.n) sum += this.grid[x + 1][y];
+    if (x - 1 >= 0) sum += this.grid[x - 1][y];
+    return sum;
+  }
+
+  diagonalSum(value: number): number {
+    let sum = 0;
+    const [x, y] = this.getIndex(value);
+    if (x + 1 < this.n && y + 1 < this.n) {
+      sum += this.grid[x + 1][y + 1];
+    }
+    if (x - 1 >= 0 && y - 1 >= 0) {
+      sum += this.grid[x - 1][y - 1];
+    }
+    if (x + 1 < this.n && y - 1 >= 0) {
+      sum += this.grid[x + 1][y - 1];
+    }
+    if (x - 1 >= 0 && y + 1 < this.n) {
+      sum += this.grid[x - 1][y + 1];
+    }
+
+    return sum;
+  }
+
+  private getIndex(value: number): [number, number] {
+    for (let i = 0; i < this.n; i++) {
+      for (let j = 0; j < this.n; j++) {
+        if (this.grid[i][j] === value) return [i, j];
+      }
+    }
+    return [0, 0];
+  }
+}
