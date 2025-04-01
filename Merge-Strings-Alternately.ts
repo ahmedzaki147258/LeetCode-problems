65bf2d4a2a50c745768804e2bd2c16b42a59bda7
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-04-01 20:46:32 +0200

    [1768] [Time Beats: 76.28%] [Memory Beats: 48.73%] - LeetPush

--- Merge-Strings-Alternately.ts
+++ Merge-Strings-Alternately.ts
@@ -2,10 +2,7 @@ function mergeAlternately(word1: string, word2: string): string {
     let res = "";
     const n = Math.min(word1.length, word2.length);
     for (let i = 0; i < n; i++) {
-        res += word1[i];
-        res += word2[i];
+        res += word1[i] + word2[i];
     }
-    res+=word1.slice(n);
-    res+=word2.slice(n);
-    return res;
+    return res + word1.slice(n) + word2.slice(n);
 };
