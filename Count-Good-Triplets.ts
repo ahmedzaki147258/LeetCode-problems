e84dda8a00bc8c9a15b94b74021bc23b02d2befb
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-04-03 01:49:03 +0200

    [1534] [Time Beats: 63.64%] [Memory Beats: 40.91%] - LeetPush

---
+++ Count-Good-Triplets.ts
@@ -0,0 +1,12 @@
+function countGoodTriplets(arr: number[], a: number, b: number, c: number): number {
+    let res = 0;
+    const n = arr.length;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            for (let k = j + 1; k < n; k++) {
+                if (Math.abs(arr[i] - arr[j]) <= a && Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) res++;
+            }
+        }
+    }
+    return res;
+};
