5b762acccb51b9e108be53ca257513e0b87cad5b
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-04-03 02:21:27 +0200

    [1290] [Time Beats: 100.00%] [Memory Beats: 71.25%] - LeetPush

---
+++ Convert-Binary-Number-in-a-Linked-List-to-Integer.ts
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function getDecimalValue(head: ListNode | null): number {
+    let str = "";
+    while(head !== null){
+        str += head.val.toString();
+        head = head.next;
+    }
+    return parseInt(str, 2);
+};
