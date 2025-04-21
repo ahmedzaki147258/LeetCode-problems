d975f37de884c14b8f6815e8249714cd4f52d297
Ahmed zaki <130254480+ahmedzaki147258@users.noreply.github.com>
2025-04-21 15:51:06 +0200

    [929] [Time Beats: 83.82%] [Memory Beats: 90.75%] - LeetPush

--- Unique-Email-Addresses.ts
+++ Unique-Email-Addresses.ts
@@ -1,7 +1,7 @@
 function numUniqueEmails(emails: string[]): number {
-    emails = emails.map(email => {
+    emails = emails.map((email: string) : string => {
         const emailParts: string[] = email.split('@');
-        const  localName = emailParts[0].split('+');
+        const  localName: string[] = emailParts[0].split('+');
         return localName[0].split('.').join('') + '@' + emailParts[1];
     });
     return (new Set(emails)).size;
